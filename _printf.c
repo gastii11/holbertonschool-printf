469c0a17a4e17577a5b6dcbed8c469a2e5c0b76b
gastii11 <10632@holbertonstudents.com>
2025-03-24 16:25:51 +0000

    implementacion base de prinft

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * _printf - Imprime valores basados en una cadena de formato
+ * @format: Lista los tipos de identificadores (c: char, s: char*, %: %%)
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	va_start (args, format);
+	count = 0;
+}
