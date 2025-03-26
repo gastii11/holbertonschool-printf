baee5c906720d86ab5ae9e06c7afd04ecf9e3fee
gastii11 <10632@holbertonstudents.com>
2025-03-26 13:32:56 +0000

    creamos main.h que tiene todos los prototipos que usamos

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef GESTIONAR_H
+#define GESTIONAR_H
+
+#include <stdarg.h>
+
+
+void gestionar_caracter(va_list *args, int *contador);
+void gestionar_cadena(va_list *args, int *contador);
+void gestionar_entero(va_list *args, int *contador);
+void gestionar_porcentaje(int *contador);
+void gestionar_invalido(char caracter_invalido, int *contador);
+
+#endif /* GESTIONAR_H */
