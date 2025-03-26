fc61c455bd1fc6d448b54d2f0a0d904eb6fbeb44
gastii11 <10632@holbertonstudents.com>
2025-03-26 13:36:59 +0000

    creamos _printf.h con la estructura que usaremos en el codigo principal y tambien prototipos

---
+++ _printf.h
@@ -0,0 +1,22 @@
+#ifndef MI_PRINTF_H
+#define MI_PRINTF_H
+
+#include <stdarg.h>
+
+/**
+ * struct GestionarFormato - Asocia un especificador con su función.
+ * @especificador: Carácter del especificador (ej: 'd', 's').
+ * @gestionador: Función que procesa el especificador.
+ */
+typedef struct GestionarFormato
+{
+	char especificador;
+	void (*gestionador)(va_list *, int *);
+} GestionarFormato;
+
+/* Prototipos */
+int _printf(const char *formato, ...);
+void entero_a_cadena(int num, char *buffer);
+void invertir_cadena(char *str);
+
+#endif /* MI_PRINTF_H */
