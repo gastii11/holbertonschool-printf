0484d682acae2d62fee6d7cf9dad03b442b743dd
gastii11 <10632@holbertonstudents.com>
2025-03-26 14:13:08 +0000

    agregamos la funcion gestionar_cadenas

--- funciones.c
+++ funciones.c
@@ -12,3 +12,21 @@ void gestionar_caracter(va_list *args, int *contador)
 	_putchar(c);
 	(*contador)++;
 }
+
+/**
+ * gestionar_cadena - Procesa el especificador %s.
+ * @args: Lista de argumentos variables.
+ * @contador: Puntero al contador de caracteres.
+ */
+void gestionar_cadena(va_list *args, int *contador)
+{
+	char *cadena = va_arg(*args, char *);
+	if (!cadena)
+		cadena = "(null)";
+	while (*cadena)
+	{
+		_putchar(*cadena);
+		cadena++;
+		(*contador)++;
+	}
+}
