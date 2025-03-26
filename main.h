23859aff9fa793acb73c808d5adaa233f9e77cec
gastii11 <10632@holbertonstudents.com>
2025-03-26 14:04:51 +0000

    creamos el archivo _putchar.c von la funcion _putchar y agregamos el prototipo a main.h

--- main.h
+++ main.h
@@ -9,5 +9,5 @@ void gestionar_cadena(va_list *args, int *contador);
 void gestionar_entero(va_list *args, int *contador);
 void gestionar_porcentaje(int *contador);
 void gestionar_invalido(char caracter_invalido, int *contador);
-
+int _putchar(char c);
 #endif /* GESTIONAR_H */
